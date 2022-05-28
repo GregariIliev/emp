68736824286ed44e94f23b4e6633e36f4380d808
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-05-28 16:55:30 +0300

    create custom table

---
+++ src/components/Body/Dashboard/Table.js
@@ -0,0 +1,35 @@
+import "./Table.css";
+
+export function Table() {
+    return (
+        <table className="table">
+            <tbody>
+                <tr>
+                    <th>Company</th>
+                    <th>Contact</th>
+                    <th>Country</th>
+                </tr>
+                <tr>
+                    <td>Alfreds Futterkiste</td>
+                    <td>Maria Anders</td>
+                    <td>Germany</td>
+                </tr>
+                <tr>
+                    <td>Centro comercial Moctezuma</td>
+                    <td>Francisco Chang</td>
+                    <td>Mexico</td>
+                </tr>
+                <tr>
+                    <td>Ernst Handel</td>
+                    <td>Roland Mendel</td>
+                    <td>Austria</td>
+                </tr>
+                <tr>
+                    <td>Island Trading</td>
+                    <td>Helen Bennett</td>
+                    <td>UK</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
