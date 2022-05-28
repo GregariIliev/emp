bfccde55d659a81b4a903a83a63437bb0de1622f
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-05-28 15:38:18 +0300

    create body

---
+++ src/components/Body/Body.js
@@ -0,0 +1,11 @@
+import './Body.css';
+
+import { Dashboard } from './Dashboard/Dashboard';
+
+export function Body(){
+    return (
+        <div className='body'>
+            <Dashboard />
+        </div>
+    )
+}
