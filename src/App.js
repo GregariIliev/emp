433af6511bd07e70c76cd8e5718656b73d74fd3d
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-05-28 15:39:01 +0300

    implement Header Body and Footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { Header } from "./components/Header/Header.js"
+import { Body } from "./components/Body/Body.js";
+import { Footer } from "./components/Footer/Footer.js"
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Body />
+      <Footer />
     </div>
   );
 }
