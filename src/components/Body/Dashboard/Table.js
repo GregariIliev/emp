1b48db5d00d9b1fc7d47c1f969e93026ff6d7cf5
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-05-29 22:44:24 +0300

    create staitc table header

--- src/components/Body/Dashboard/Table.js
+++ src/components/Body/Dashboard/Table.js
@@ -5,6 +5,10 @@ export function Table() {
         <table className="table">
             <tbody>
                 <tr>
+                    <th>Employee ID #1</th>
+                    <th>Employee ID #2</th>
+                    <th>Project ID</th>
+                    <th>Worked days</th>
                 </tr>
             </tbody>
         </table>
