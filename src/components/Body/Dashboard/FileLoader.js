a924c51411be043df61656e55fb0dc5e230e2ea4
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-05-29 22:39:42 +0300

    create csv converter to prijects

--- src/components/Body/Dashboard/FileLoader.js
+++ src/components/Body/Dashboard/FileLoader.js
@@ -21,6 +21,59 @@ export function FileLoader({
         const csvFiles = Array.from(e.target.files);
 
     }
+
+    const csvConvertToProjects = (files) => {
+        files.forEach(async (file) => {
+            const plainText = await file.text();
+
+            const rows = plainText.trim().split('\r\n');
+
+            const splitedRows = rows.map(r => r.split(', '));
+
+            const projects = [];
+
+            splitedRows.forEach(row => {
+
+                const firstDate = new Date(row[2]).getTime();
+
+                let secondDate = new Date(row[3]).getTime();
+
+                if (isNaN(secondDate)) {
+                    secondDate = Date.now();
+                }
+
+                const milliseconds = secondDate - firstDate;
+
+                const days = Math.round(milliseconds / (1000 * 3600 * 24));
+
+
+                const index = projects.findIndex(project => project.id === row[1]);
+
+                if (index === -1) {
+                    projects.push({
+                        id: row[1],
+                        employees: [{
+                            id: row[0],
+                            workDays: days
+                        }]
+                    });
+
+                }else {
+                    projects[index].employees.push({
+                        id: row[0],
+                        workDays: days
+                    });
+                }
+            });
+
+            projects.forEach(p => {
+                p.employees.sort((e1, e2) => e2.workDays - e1.workDays);
+            });
+            
+        })
+    }
+
+
     return (
         <div className="drag-area"
             onDrop={e => handleDrop(e)}
