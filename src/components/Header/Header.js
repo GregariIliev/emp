a4c7f7a73162445bf3c6d41eeafe2b5a05932e90
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-05-30 02:40:14 +0300

    create anchor buttons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,10 @@ import './Header.css';
 export function Header(){
     return (
         <header className='header'>
-            
+            <nav className='navbar'>
+                <a href='/home'>Home</a>
+                <a href='/dashboard'>Dashboard</a>
+            </nav>
         </header>
     )
 }
