767cd90de88c15871d238b3546a4ec75d64cb7cf
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-05-29 22:44:08 +0300

    remove static template table

--- src/components/Body/Dashboard/Table.js
+++ src/components/Body/Dashboard/Table.js
@@ -5,29 +5,6 @@ export function Table() {
         <table className="table">
             <tbody>
                 <tr>
-                    <th>Company</th>
-                    <th>Contact</th>
-                    <th>Country</th>
-                </tr>
-                <tr>
-                    <td>Alfreds Futterkiste</td>
-                    <td>Maria Anders</td>
-                    <td>Germany</td>
-                </tr>
-                <tr>
-                    <td>Centro comercial Moctezuma</td>
-                    <td>Francisco Chang</td>
-                    <td>Mexico</td>
-                </tr>
-                <tr>
-                    <td>Ernst Handel</td>
-                    <td>Roland Mendel</td>
-                    <td>Austria</td>
-                </tr>
-                <tr>
-                    <td>Island Trading</td>
-                    <td>Helen Bennett</td>
-                    <td>UK</td>
                 </tr>
             </tbody>
         </table>
