8ec36b6de3257ae4ab8c10e07e5238253ee07508
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-05-30 03:19:41 +0300

    add anchors to table

--- src/components/Body/Dashboard/Table.js
+++ src/components/Body/Dashboard/Table.js
@@ -27,10 +27,10 @@ export function Table({
                 {table
                     ? tableData.map(project => (
                         <tr key={project.id}>
-                            <td>{project.employees[0].id}</td>
-                            <td>{project.employees[1].id}</td>
-                            <td>{project.id}</td>
-                            <td>{project.employees[0].workDays + project.employees[1].workDays}</td>
+                            <td><a href={'/employee/' + project.employees[0].id}>{project.employees[0].id}</a></td>
+                            <td><a href={'/employee/' + project.employees[1].id}>{project.employees[1].id}</a></td>
+                            <td><a href={'/project/' + project.id}>{project.id}</a></td>
+                            <td className="workDays">{project.employees[0].workDays + project.employees[1].workDays}</td>
                         </tr>
                     ))
                     : null
