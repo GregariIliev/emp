72b8349cb8c2c58509205524f3d6cbd93167feb6
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-05-30 03:21:32 +0300

    remove home anchor

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,6 @@ export function Header(){
     return (
         <header className='header'>
             <nav className='navbar'>
-                <a href='/home'>Home</a>
                 <a href='/dashboard'>Dashboard</a>
             </nav>
         </header>
