0ba8cb7fedc8e916e2bf83d1c5eb236d63be175f
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-05-30 03:54:14 +0300

    format code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Header } from "./components/Header/Header.js"
 import { Body } from "./components/Body/Body.js";
 import { Footer } from "./components/Footer/Footer.js"
 
+
+
 function App() {
   return (
     <div className="App">
