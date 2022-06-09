d5c026f1c6c155292fbde4bbc558d09ff20b56d4
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-09 08:18:21 +0300

    add title

--- src/components/Body/Dashboard/Dashboard.js
+++ src/components/Body/Dashboard/Dashboard.js
@@ -11,11 +11,13 @@ export function Dashboard() {
     const [projectsData, setProjectsData] = useState();
 
     const getProjects = (value) => {
+
         setProjectsData(value);
     }
 
     return (
         <div className="dashboard">
+            <h1>Pair of employees who have worked together on common projects for the longest period of time</h1>
             <Table tableData={projectsData} />
             <FileLoader getProjects={getProjects} />
         </div>
